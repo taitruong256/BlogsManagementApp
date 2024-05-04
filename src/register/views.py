db1d45948134161275408db66abb93a6b3c61847
unknown <lamquangphu2176@gmail.com>
2024-05-05 03:51:19 +0700

    update: register, notiface

--- src/register/views.py
+++ src/register/views.py
@@ -3,17 +3,30 @@ from django.shortcuts import render
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Profile
 from django.urls import include, reverse_lazy, path
 from django.shortcuts import render, redirect
 from django.views import View
-
-
+from django.contrib import messages
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    fullname = forms.CharField(required=True)
+    gender = forms.ChoiceField(choices=Profile.GENDER_CHOICES)
+    birthdate = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ['username', 'fullname', 'email', 'password1', 'password2', 'gender', 'birthdate']
 
+    def save(self, commit=True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+            user.profile.fullname = self.cleaned_data['fullname']
+            user.profile.gender = self.cleaned_data['gender']
+            user.profile.birthdate = self.cleaned_data['birthdate']
+            user.profile.save()
+        return user
 
 
 
@@ -23,11 +36,17 @@ class RegisterView(View):
         return render(request, 'register/register.html', {'form': form})
 
     def post(self, request):
+      
         form = RegisterForm(request.POST)
+        
         if form.is_valid():
+            messages.success(request, "Đăng ký thành công! Vui lòng đăng nhập.")
             user = form.save()
-            # Có thể thêm logic để tự động đăng nhập người dùng tại đây
-            return redirect('login')  # Chuyển hướng đến trang đăng nhập sau khi đăng ký thành công
+            # Chuyển hướng người dùng đến trang đăng nhập sau khi đăng ký thành công
+            return redirect('login')  # Đảm bảo rằng bạn có URL với tên 'login' được định nghĩa trong urls.py
+        # Nếu form không hợp lệ, hiển thị lại form cùng với thông tin đã điền và lỗi
+        
         return render(request, 'register/register.html', {'form': form})
+
 
 
